9331ceb38572590daa919b334dbb5ea00a681966
AbdelrahmanAtia <abdelrahmanattya94@gmail.com>
2019-06-08 06:18:40 +0200

    first commit

---
+++ cms-back-end/src/main/java/org/javaworld/cmsbackend/CmsBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.javaworld.cmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsBackEndApplication.class, args);
+	}
+
+}
